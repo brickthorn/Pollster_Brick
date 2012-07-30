cdc216b53c99c04be57980d3213803088e5d4d54
DBC Apprentice <apprentice@devbootcamp.com>
2012-07-30 10:49:31 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrickPollster::Application.config.secret_token = '3f659a84d0f11c6363b7646952499bfbc2c7215dbae3430b1e727709ce9e603c8f4d7cfb3129da0c18abe01bbd41814a911c04ae85934354c1ad04ea35eab6db'
